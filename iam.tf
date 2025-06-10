6c8e5336b5ba43ef465c21b863d92643bfe5e774
gon-papa <naoki.0821g@gmail.com>
2025-06-11 01:07:30 +0900

    elbとiamとs3を構築

---
+++ iam.tf
@@ -0,0 +1,49 @@
+# iam ロール
+resource "aws_iam_instance_profile" "app_ec2_profile" {
+  name = aws_iam_role.app_iam_role.name
+  role = aws_iam_role.app_iam_role.name
+}
+
+resource "aws_iam_role" "app_iam_role" {
+  name               = "${var.project}-${var.environment}-app-iam-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+}
+
+# ポリシーのアタッチ
+# EC2のタグ情報を取得するためのポリシー
+resource "aws_iam_role_policy_attachment" "app_iam_role_ec2_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
+}
+
+# セッションマネージャーを使った接続を行うためのポリシー
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_managed" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# S3のファイル取得をするためのポリシー
+resource "aws_iam_role_policy_attachment" "app_iam_role_S3_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+# パラメータストアから環境変数を取得するためのポリシー
+resource "aws_iam_role_policy_attachment" "app_iam_role_ssm_readonly" {
+  role       = aws_iam_role.app_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
+}
+
+
+
+# 信頼ポリシー作成
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
